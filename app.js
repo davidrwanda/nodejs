df1b6cb1b9570243b3a6cb7d2ee6e8b775cd4024
davidrwanda <daventamag1@gmail.com>
2021-06-28 15:03:18 +0200

    html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
   const express = require('express');
   
   const bodyParser = require('body-parser');
+  
 
   const app = express();
 
@@ -9,6 +10,7 @@
   const shopRoutes = require('./routes/shop');
 
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.use('/admin', adminRoutes);
   app.use(shopRoutes);
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
  router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" id=""><button type="submit">Add</button></form>')
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
   });
 
   router.post('/add-product', (req, res, next) => {
